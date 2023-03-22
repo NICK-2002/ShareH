8f1367aba933dc47f1e2ae27b7e8d27cc96c4942
Nikhil <nikhilsinghshekhawat6378@gmail.com>
2023-03-23 00:30:10 +0530

    created the ui for audio history

--- lib/app/modules/historyModule/mainHistoryPage/views/audio_history_view.dart
+++ lib/app/modules/historyModule/mainHistoryPage/views/audio_history_view.dart
@@ -10,27 +10,74 @@ class AudioHistoryView extends GetView<HistoryController> {
   Widget build(BuildContext context) {
     return GetBuilder<HistoryController>(builder: (controller) {
       return Scaffold(
-        body: Center(
-            child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(
-              height: Get.width * 0.2,
-            ),
-            Container(
-                width: Get.width,
-                height: Get.height * 0.4,
-                child: Image.asset(
-                  "assets/history_empty_view/emptyaudioView.png",
-                  fit: BoxFit.fill,
-                )),
-            TextView(
-              text: "No Audio(s) Received",
-              size: 21,
-              color: Color.fromARGB(255, 168, 167, 167),
-            )
-          ],
-        )),
+        body: controller.user.isEmpty
+            ? Center(
+                child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: Get.width * 0.2,
+                  ),
+                  Container(
+                      width: Get.width,
+                      height: Get.height * 0.4,
+                      child: Image.asset(
+                        "assets/history_empty_view/emptyaudioView.png",
+                        fit: BoxFit.fill,
+                      )),
+                  const TextView(
+                    text: "No Photos(s) Received",
+                    size: 21,
+                    color: Color.fromARGB(255, 168, 167, 167),
+                  )
+                ],
+              ))
+            : Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: controller.audio.isNotEmpty
+                    ? GridView.builder(
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                                crossAxisCount: 3),
+                        itemCount: controller.audio.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return GestureDetector(
+                            onTap: () {},
+                            child: Card(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(
+                                      12.0), //<-- SEE HERE
+                                ),
+                                child: GridTile(
+                                  footer: const Center(
+                                      child: TextView(
+                                    text: "audioFile",
+                                    color: Colors.white,
+                                  )),
+                                  child: Container(
+                                    decoration: const BoxDecoration(
+                                      borderRadius:
+                                          BorderRadius.all(Radius.circular(10)),
+                                      gradient: LinearGradient(
+                                          begin: Alignment.centerRight,
+                                          end: Alignment.centerLeft,
+                                          colors: <Color>[
+                                            Color.fromARGB(255, 157, 189, 237),
+                                            Color.fromARGB(255, 218, 81, 248),
+                                            Color.fromARGB(255, 241, 167, 247),
+                                          ]),
+                                    ),
+                                    child: Center(
+                                        child: Image.asset(
+                                            "assets/icon/audioIcon.png")),
+                                  ),
+                                )),
+                          );
+                        })
+                    : const Center(
+                        child: CircularProgressIndicator(),
+                      ),
+              ),
       );
     });
   }
